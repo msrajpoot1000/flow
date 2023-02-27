edd68aabd5de501cff64d91594ef82789da9eb58
msrajpoot1000 <msrajpoot1000@gmail.com>
2023-02-28 02:03:10 +0530

    Updates

---
+++ precache-manifest.054ef0f939e9db8db1f69fe7f7261ba5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0da27b9dedabb8a9e0c7",
+    "url": "/static/css/main.983e3132.chunk.css"
+  },
+  {
+    "revision": "0da27b9dedabb8a9e0c7",
+    "url": "/static/js/main.0da27b9d.chunk.js"
+  },
+  {
+    "revision": "62cac4abb9d6098befd4",
+    "url": "/static/js/1.62cac4ab.chunk.js"
+  },
+  {
+    "revision": "730a94857ff4ea951266",
+    "url": "/static/css/2.d8088ecf.chunk.css"
+  },
+  {
+    "revision": "730a94857ff4ea951266",
+    "url": "/static/js/2.730a9485.chunk.js"
+  },
+  {
+    "revision": "6a532cb34d7e88a4030d",
+    "url": "/static/js/runtime~main.6a532cb3.js"
+  },
+  {
+    "revision": "81a9e4205f22225865220596c89cf221",
+    "url": "/static/media/fontPage2.81a9e420.jpg"
+  },
+  {
+    "revision": "42ef0c1b3e835b239c3bec0f200a9894",
+    "url": "/index.html"
+  }
+];
